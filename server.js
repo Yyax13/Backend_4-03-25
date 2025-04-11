8ab889cefe43f6935923ddac346c68f3d14e809e
Yyax13 <Yyax13@proton.me>
2025-04-11 19:33:31 +0000

    corrige a função tome para acessar corretamente o último item do jogador e adiciona logs de depuração

--- server.js
+++ server.js
@@ -438,6 +438,7 @@ async function tome(Spell) {
             return result;
         } else if (Spell.target) {
             const Target = Spell.target; // Corrigido para usar `Spell.target`
+            console.log(Target)
             const Player = await searchPlayer(Target); // Adicionado await
             const LastItem = Player.others.LastItemID; // Corrigido para acessar corretamente
             await levelUp(Spell.caller); // Adicionado await
